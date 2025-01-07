aa9aac40116059887dd52e750fee5ccf59acf3fe
William <wpelletier.development@yahoo.com>
2025-01-07 12:18:46 -0500

    Move renderable object to core

--- module/module.go
+++ module/module.go
@@ -14,34 +14,4 @@ type Model interface {
 
 
 
-// TODO: they should have an renderable interface
-type Renderable interface {}
-
-
-
-type Text struct {
-    Data                string
-    GraphicMode         uint
-    ForegroundColor     uint
-    BackgroundColor     uint
-    relativePosition    core.Position
-}
-
-func NewText(data string, gmode uint, fclr uint, bclr uint, row int, col int) Text {
-    return Text{
-        Data: data,
-        GraphicMode: gmode,
-        ForegroundColor: fclr,
-        BackgroundColor: bclr,
-        relativePosition: core.Position{ Row: row, Col: col},
-    }
-}
-
-
-type ProgressBar struct {
-    Percent     float32
-    FullColor   uint
-    EmptyColor  uint
-    Length      uint
-}
 
