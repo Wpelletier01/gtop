48cc3b5e32097b09302bae966c00dc836d064c23
william <wpelletier.development@yahoo.com>
2025-01-08 15:47:21 -0500

    clear interface model method

--- module/model.go
+++ module/model.go
@@ -1,15 +1,10 @@
 package module;
 
-import "gtop/core"
 
 type Model interface {
     
-    Init()
-    Collect()
-    // Refresh data
-    Update()
-    // The terminal has been resized
-    Resize(s core.Size)
+    DrawStatic()
+    
 }
 
 
